b85a444b45ea17efca91ec0661c7a2085deed9a0
jiyeonCoder <jiyeon.choi218@gmail.com>
2023-10-06 09:17:30 +0900

    update users urls

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from users import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('login/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('signup/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+]
