92f4aed7fc97cfb50056ecb84afeeafe8d944259
jiyeonCoder <jiyeon.choi218@gmail.com>
2023-10-07 22:57:44 +0900

    created Post model and setup static files

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(blank=True, upload_to='%Y/%m/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    like_count = models.ManyToManyField(User, related_name='like_posts')
+
+    def __str__(self):
+        return str(self.title)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status, permissions
+from rest_framework.response import Response
 
-# Create your views here.
+
+class PostView(APIView):
+    def get(self, request):
+        pass
+    
+    def post(self, request):
+        pass
+
+class PostDetailView(APIView):
+    def get(self, request):
+        pass
+    
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
