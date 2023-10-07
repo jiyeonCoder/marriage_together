8fa2f59e6ce9db1f83dd9273d55209927851db7b
jiyeonCoder <jiyeon.choi218@gmail.com>
2023-10-07 15:33:41 +0900

    simplejwt basic settings in settings.py

--- marry_together/settings.py
+++ marry_together/settings.py
@@ -42,6 +42,14 @@ INSTALLED_APPS = [
     'posts',
 ]
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
