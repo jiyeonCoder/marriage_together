e9c55509dce8f0f086f6adf488d2530f09acad49
jiyeonCoder <jiyeon.choi218@gmail.com>
2023-10-08 21:55:02 +0900

    test

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,27 @@
 from rest_framework.views import APIView
 from rest_framework import status, permissions
 from rest_framework.response import Response
+from posts.models import Post
+from posts.serializers import PostSerializer, PostCreateSerializer
 
 
 class PostView(APIView):
     def get(self, request):
-        pass
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     def post(self, request):
-        pass
+        if request.user.is_authenticated:
+            serializer = PostCreateSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save(user=request.user)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'error': 'User not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
+
 
 class PostDetailView(APIView):
     def get(self, request):
--- users/views.py
+++ users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
+from rest_framework_simplejwt.views import TokenObtainPairView
+from users.models import User
 
-# Create your views here.
+from users.serializers import UserSerializer, CustomTokenObtainPairSerializer
+
+
+class UserView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response("가입완료!", status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+
+class MockView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request):
+        print(request.user)
+        return Response("get 요청")
