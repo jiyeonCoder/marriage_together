589b9b90af10a364438be1d59c682ffadbd4a93e
jiyeonCoder <jiyeon.choi218@gmail.com>
2023-10-10 03:45:42 +0900

    created signup, login, main page

---
+++ frontend/index.js
@@ -0,0 +1,11 @@
+console.log(":)")
+
+
+window.onload=()=>{
+    const payload = localStorage.getItem("payload");
+    const payload_parse = JSON.parse(payload)
+    console.log(payload_parse.email)
+
+    const intro = document.getElementById("intro")
+    intro.innerText = payload_parse.email
+}
