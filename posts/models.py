bbea1faccc8ce62d015377f2600f1dd291f96c11
Kim-Minjae9872 <141595332+Kim-Minjae9872@users.noreply.github.com>
2023-10-10 09:56:27 +0900

    comment model,view base

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-
+class Comment(models.Model):
+    author = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-class Comment:
-    pass
+class Comment(APIView):
+    def get(self, request):
+        """post의 댓글을 보여줍니다."""
+
+    def post(self, request):
+        """각 post에 댓글을 생성합니다."""
+
+    def put(self, request):
+        """댓글을 수정"""
+
+    def delete(self, request):
+        """댓글을 삭제"""
