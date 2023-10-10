1832297c192c7703cebc9be9feb8898aee73ada6
Kim-Minjae9872 <141595332+Kim-Minjae9872@users.noreply.github.com>
2023-10-10 22:01:37 +0900

    10/10 작업

--- posts/models.py
+++ posts/models.py
@@ -16,8 +16,11 @@ class Post(models.Model):
     
 
 class Comment(models.Model):
-    author = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name='작성자')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='포스트', related_name='post_comment')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.content)
--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,8 @@ from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
 from posts.models import Post
-from posts.serializers import PostSerializer, PostCreateSerializer
+from posts.models import Comment
+from posts.serializers import CommentSerializer, PostSerializer, PostCreateSerializer
 
 
 class PostView(APIView):
@@ -53,15 +54,57 @@ class PostDetailView(APIView):
             return Response({'error': '권한이 없습니다.'}, status=status.HTTP_403_FORBIDDEN)
         
 
-class Comment(APIView):
+class CommentView(APIView):
     def get(self, request):
-        """post의 댓글을 보여줍니다."""
+        # author = request.data.get('author')
+        # post = request.data.get('post')
+        # post = get_object_or_404(Post, id=post_id)
+        # comments = post.comment_set.all()
+        # serializer = CommentSerializer(comments, many=True)
+        # return Response(serializer.data, status=status.HTTP_200_OK)
+        """post의 댓글을 보여줍니다.  =>  PostSerializer에서 CommentSerializaer한 값을 가져옵니다. """ 
 
     def post(self, request):
+        """
+        author = request.user
+         data = {
+             "content": request.POST.get('content', None),
+             "author": author.id
+         }
+        serializer = CommentSerializer(data=data)
+        """
+
+        """
+        author = User.objects.get(pk=request.user)
+        serializer = CommentSerializer(data=request.data, context={"author":author.id})
+        """
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         """각 post에 댓글을 생성합니다."""
 
-    def put(self, request):
+    def put(self, request, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)
+        serializer = CommentSerializer(comment, data=request.data)
+        if request.user == comment.user:
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response("권한이 없습니다.", status=status.HTTP_403_FORBIDDEN)
         """댓글을 수정"""
 
-    def delete(self, request):
+    def delete(self, request, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)
+        if request.user == comment.user:
+            comment.delete()
+            return Response({'게시글이 삭제되었습니다.'}, status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response({'error': '권한이 없습니다.'}, status=status.HTTP_403_FORBIDDEN)
+        
         """댓글을 삭제"""
