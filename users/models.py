eeaa73968537de46bc355fc62cb8fb5f4ed1f970
lazycaterpillar <absolem.jk@gmail.com>
2023-10-10 09:21:49 +0800

    user create

--- users/models.py
+++ users/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None):
+        if not username:
+            raise ValueError("Users must have a username")
+        user = self.model(
+            username = username,
+        )
+        print("가입")
+        
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=20, unique=True)
+    fullname = models.CharField(max_length=20, blank=True)
+    password = models.CharField(max_length=20)
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True,
+    )
+    followings = models.ManyToManyField("self", symmetrical=False, related_name="followers", blank=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
--- users/views.py
+++ users/views.py
@@ -1,3 +1,71 @@
 from django.shortcuts import render
+from django.db.models import Q
+from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 
-# Create your views here.
+from users.models import User
+from users.serializers import CustomTokenObtainPairSerializer, UserSerializer, UserProfileSerializer
+from articles.models import Article
+from articles.serializers import ArticleSerializer
+
+class UserView(APIView):
+    #사용자 정보 조회
+    def get(self, request):
+        return Response(UserSignupSerializer(request.user).data, status=status.HTTP_200_OK)
+
+    #회원가입
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response("message: User created successfully")
+        else:
+            return Response({"message": f"${serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    #회원 정보 수정
+    def put(self, request):
+        return Response("message: User updated successfully")
+
+    #회원 탈퇴
+    def delete(self, request):
+        return Response("message: User deleted successfully")
+        
+
+
+class FollowView(APIView):
+    def post(self, request, user_id):
+        you = get_object_or_404(User, id=user_id)
+        me = request.user
+        if me in you.followers.all():
+            you.followers.remove(me)
+            return Response("message: 언팔로우", status=status.HTTP_200_OK)
+        else:
+            you.followers.add(me)
+            return Response("message: 팔로우", status=status.HTTP_200_OK)
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+
+class mockView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        print(request.user)
+        user = request.user
+        user.is_admin = True
+        user.save()
+        return Response("message: get 요청")
+
+
+class ProfileView(APIView):
+    def get(self, request, user_id):
+        user = get_object_or_404(User, id=user_id)
+        serializer = UserProfileSerializer(user)
+        return Response(serializer.data)
